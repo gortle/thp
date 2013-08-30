a234a3c1e00ca037528423fc6875bacf4e09e2e1
gortle <chrishudson777@gmail.com>
2013-08-30 11:31:37 +1000

    first load
    playing around

---
+++ thp/Controllers/MainController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace thp.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            return View();
+        }
+
+    }
+}
